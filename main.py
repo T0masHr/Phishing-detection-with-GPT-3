467e6d9b0f192f3fb652da8c5a2b02ee90b5abc5
T0masHr <hrubec.tomas@outlook.com>
2022-05-31 11:54:22 +0200

    read textfile

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import readEmail
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +14,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    readEmail.open_message(r"C:\Users\tomas\OneDrive - Duale Hochschule Baden-Württemberg Stuttgart\Projekt\EmailFiles\Gremienwahlen.txt")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
