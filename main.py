453ce89d71f58e9cf9b017cc7653b66129709147
T0masHr <hrubec.tomas@outlook.com>
2022-06-01 04:07:13 +0200

    - added first arg(debug toggle) - todo notes

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import logging
+import argparse
+import openai
+# import os
 # Import the email modules we'll need
 from email.message import EmailMessage
-# import os
-import openai
-import logging
 
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = "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"
+openai.api_key = "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"  # the key should be loaded from envvar # os.getenv("OPENAI_API_KEY")
+
+
+# TODO: -
+#  implement additional args:
+#  1. - the ability to disable the actual api call to openai to save credits;
+#  2. - provide the file path per arg instead of stdin;
+#  3. - supply the openai API_KEY with arg;
+#  extract the actual "text" field from the api response, maybe display the whole response when logging is set to debug
+#  add the ability to check multiple files at once or rather to supply a folder with files to check
+
 
 
 def open_message(textfile):
@@ -13,9 +23,10 @@ def open_message(textfile):
     # Open the plain text file whose name is in textfile for reading.
     with open(textfile) as fp:
         # Create a text/plain message
-        text_from_file = fp.read()
+        text_from_file = f'''{fp.read()}'''
         return text_from_file
 
+        # handling the file as email message using the email module:
         # msg = EmailMessage()
         # msg.set_content(readText)
         # msg_string = msg.as_string()
@@ -23,31 +34,15 @@ def open_message(textfile):
         # return msg_string
 
 
-def api_call_completion(msg_input):
-    logging.debug(f'Called function "{api_call_completion.__name__}" and using "{msg_input}" as its parameter')
+def api_call_completion(prompt, msg_input):
+    logging.debug(f'Called function "{api_call_completion.__name__}"')
+    logging.debug(f'First parameter is: "{prompt}"')
+    logging.debug(f'Second parameter is: "{msg_input}"')
+
     # call to the openai web api with the supplied email text body as input
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="Determine if this email is a phishing email:\n\n" + msg_input,
-        # prompt="Determine if this email is a phishing email:\n\nDear Kelly,\n\nIt was great to talk to you at the seminar."
-        #        "I thought Jane's talk was quite good.\n\nThank you for the book. Here's my address 2111 Ash Lane, "
-        #        "Crestview CA 92002\n\nBest,\n\nMaya\n\n",
-        # prompt="Determine if this email is a phishing email:\nLiebe Mitarbeiter,\n \nBeachten Sie dieses wichtige "
-        #        "Update, dass unser neues Webmail mit einem neuen \nMessaging-System von Microsoft verbessert wurde, "
-        #        "das auch eine schnellere Nutzung \nvon E-Mails, freigegebenen Kalendern, Webdokumenten und der neuen "
-        #        "Anti-Spam-\nVersion 2021 umfasst. \n \nBitte verwenden Sie den unten stehenden Link, um Ihr Microsoft "
-        #        "Web-Mail-\nBenutzerauthentifizierungsformular 2021 auszufüllen. \n \nKLICKEN Sie auf Outlook Web "
-        #        "Access, um sofort zu aktualisieren. \n \nIT Service Center (Rechenzentrum) - DHBW Stuttgart \n© "
-        #        "Copyright 2021 - DHBW Stuttgart. Alle Rechte vorbehalten.\n\n",
-        # Bei so langen Nachrichten können einfach drei Hochkommas genutzt gerden um die Nachricht einfacher als String einzugeben
-        # '''
-        # Hier
-        # kann
-        # die
-        # Nachricht
-        # einfach
-        # rein
-        # '''
+        prompt=prompt + msg_input,
         temperature=0.1,
         max_tokens=256,
         top_p=1,
@@ -59,29 +54,42 @@ def api_call_completion(msg_input):
 
 #######################################################################################################################
 def main():
+    # command line arguments handling:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-debug', action='store_true', help='Turn DEBUG output ON')
+    options = parser.parse_args()
+
+    if options.debug is True:
+        logging.basicConfig(level=logging.DEBUG)
+        # Print the Library's installation path
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
     logging.info('Starting...')
     logging.debug(f'Called function "{main.__name__}"')
 
     path = input("Enter the path to email file: \n")
 
-    if not path:
+    if not path:  # check if user input is empty, if it is, use following hardcoded path:
         logging.warning('Empty input, falling back to default path')
-        path = r"emailMessages\asString_placeholderMessage.txt"  # hardcoded path for dev
+        path = r"emailMessages\placeholderMessage.txt"  # hardcoded path here
 
-    logging.info(f'Using the following path:  {path}')
+    logging.info(f'Using the following path:  {path} \n')
 
+    msg_to_check = open_message(path)  # call the function to open file at supplied path
+    logging.info('Opening message completed, following text was read: \n')
+    print(msg_to_check)  # print the text back to user
 
-    msg_to_check = open_message(path)
-    print(msg_to_check)
+    default_api_prompt = "Determine if this email is a phishing email:\n\n"
 
-    # api_response = api_call_completion(msg_to_check)
+    # following is responsible for the api call, disabled to save credits:
+    # api_response = api_call_completion(default_api_prompt, msg_to_check)
     # print("API response: \n")
     # print(api_response)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    # log level is now set using cmd args
+    # logging.basicConfig(level=logging.DEBUG) # set the logging level, DEBUG displays all messages, INFO is less chatty
     main()
-
-
