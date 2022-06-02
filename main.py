47d43fa400ed4241efa80f1a6013f14e01a81513
T0masHr <hrubec.tomas@outlook.com>
2022-06-02 12:34:23 +0200

    - separate file for constant values used during development

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import logging
 import argparse
 import openai
+from pathlib import Path
 
-import chatbot # import file with chatbot functions
+import chatbot  # import file with chatbot functions
 # import os
 # Import the email modules we'll need
 from email.message import EmailMessage
+import dev_defaults
 
-openai.api_key = "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"  # the key should be loaded from envvar # os.getenv("OPENAI_API_KEY")
-default_path = "emailMessages\placeholderMessage.txt"
+openai.api_key = dev_defaults.API_KEY  # the key should be loaded from envvar # os.getenv("OPENAI_API_KEY")
+DEFAULT_PATH = dev_defaults.DEFAULT_PATH
 
 
 # TODO: -
@@ -19,20 +21,35 @@ default_path = "emailMessages\placeholderMessage.txt"
 #  extract the actual "text" field from the api response, maybe display the whole response when logging is set to debug
 #  add the ability to check multiple files at once or rather to supply a folder with files to check
 
+def open_path(supplied_path):
+    logging.debug(f"Called function '{open_path.__name__}' and using '{supplied_path}' as its parameter")
+
+    path = Path(supplied_path)
+    logging.info(f"Using the following path:  {path} \n")
+
+    msg_to_check = open_message(path)  # call the function to open file at supplied path
+    logging.info("Opening message completed, following text was read: \n")
+    return msg_to_check
+
 
 def open_message(textfile):
-    logging.debug(f'Called function "{open_message.__name__}" and using "{textfile}" as its parameter')
+    logging.debug(f"Called function '{open_message.__name__}' and using '{textfile}' as its parameter")
     # Open the plain text file whose name is in textfile for reading.
-    with open(textfile) as fp:
-        # Create a text/plain message
-        text_from_file = f'''{fp.read()}'''
-        return text_from_file
+
+    try:
+        with open(textfile) as fp:
+            # Create a text/plain message
+            text_from_file = f'''{fp.read()}'''
+            return text_from_file
+    except FileNotFoundError:
+        logging.critical(f"!!! THE FILE {textfile} WAS NOT FOUND, ABORTING EXECUTION !!!")
+        exit()
 
 
 def api_call_completion(prompt, msg_input):
-    logging.debug(f'Called function "{api_call_completion.__name__}"')
-    logging.debug(f'First parameter is: "{prompt}"')
-    logging.debug(f'Second parameter is: "{msg_input}"')
+    logging.debug(f"Called function '{api_call_completion.__name__}'")
+    logging.debug(f"First parameter is: '{prompt}'")
+    logging.debug(f"Second parameter is: '{msg_input}'")
 
     # call to the openai web api with the supplied email text body as input
     response = openai.Completion.create(
@@ -45,6 +62,11 @@ def api_call_completion(prompt, msg_input):
         presence_penalty=0
     )
     return response
+
+
+def demo_devmode():
+    logging.warning("DEMO MODE ACTIVE")
+    path = DEFAULT_PATH  # hardcoded path here
 
 
 def setup_logging(verbosity_level):
@@ -63,32 +85,22 @@ def main():
                         help="set output verbosity: -v for INFO; -vv for DEBUG")
     parser.add_argument("--enable_api", action="store_true", help="enable the call to openai api")
     parser.add_argument("--demo", action="store_true", help="use demo values")
-    parser.add_argument("path", help="uses this string as path to single file", default=default_path)  # this arg is required
-    parser.add_argument("--chatbot", action="store_true", help="switch to chatbot mode")
-    options = parser.parse_args()
+    parser.add_argument("path", nargs="?", default=DEFAULT_PATH, help="uses this string as path to single file")
+    parser.add_argument("api_key", nargs="?", help="API_KEY for openai")
 
-    setup_logging(options.verbosity_level) # call the function to set up logging with provided verbosity level
-    if options.chatbot:
-        chatbot()
+    # parser.add_argument("--chatbot", action="store_true", help="switch to chatbot mode")
+    options = parser.parse_args()
+    setup_logging(options.verbosity_level)  # call the function to set up logging with provided verbosity level
 
     if options.demo:  # check if user input is empty, if it is, use following hardcoded path:
-        logging.warning('Empty input, falling back to default path')
-        path = default_path  # hardcoded path here
-    else:
-        path = options.path
+        demo_devmode()
+
     # END of args handling          #############################################
 
-    logging.info('Starting...')
-    logging.debug(f'Called function "{main.__name__}"')
+    logging.info("Starting...")
+    logging.debug(f"Called function '{main.__name__}'")
 
-    # path = input("Enter the path to email file: \n")
-
-
-
-    logging.info(f'Using the following path:  {path} \n')
-
-    msg_to_check = open_message(path)  # call the function to open file at supplied path
-    logging.info('Opening message completed, following text was read: \n')
+    msg_to_check = open_path(options.path)
     print(msg_to_check)  # print the text back to user
 
     default_api_prompt = "Determine if this email is a phishing email:\n\n"
@@ -102,8 +114,5 @@ def main():
         print("no actual api call performed to save credits. \n use argument '--enable_api' to use openai's api ")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # log level is now set using cmd args
-    # logging.basicConfig(level=logging.DEBUG) # set the logging level, DEBUG displays all messages, INFO is less chatty
     main()
