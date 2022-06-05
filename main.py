ce40331fada2b68bd7b9a61494a29661e145c81c
T0masHr <hrubec.tomas@outlook.com>
2022-06-05 20:58:19 +0200

    multiple functions added

--- main.py
+++ main.py
@@ -26,40 +26,47 @@ def get_paths_list(supplied_path_list: list) -> list:
     logging.debug(f"Called function '{get_paths_list.__name__}'")
     logging.debug(f"Your list contains the following items:\n{chr(10).join(supplied_path_list)}\n")
     logging.debug(f"The parameter has the type {type(supplied_path_list)}")
-    path_list = []
+    path_list = []  # user supplied paths
+    files_list = []  # fill with only file paths and no folder paths
 
-    if len(supplied_path_list) == 0:
+    if len(supplied_path_list) == 0:  # The supplied list should never be empty
         logging.critical("Supplied path is empty, aborting")
-        exit("No path supplied")
+        exit("EXIT: No path supplied")
 
     if len(supplied_path_list) >= 1:
-        logging.debug(f"The following list of path objects is being appended {path_list}")
-        for i in supplied_path_list:
+        logging.debug(f"The following list of path objects is being appended {path_list} (it should be empty here)")
+        for i in supplied_path_list:  # loop over the whole list
             p = Path(i)  # create Path object from one item in list
             path_list.append(p)  # append the newly created Path to the path list
 
         logging.debug(f"Following list with paths was created: \n{path_list}\n")
         logging.debug(f"Following list with paths was created: \n{chr(10).join(map(str, path_list))}\n")
 
-    # TODO: check if a path is a folder path, if it is get all files in the folder and return paths to single files
-    # TODO: after this implemented, this function should return a list of all filepaths
-    for i in path_list:
-        if i.is_file():
-            logging.debug(f"The path '{i}' is a file path")
+        logging.debug(f"Checking if the paths are files or directories")
 
-        if i.is_dir():
-            logging.debug(f"The path '{i}' is a folder path")
+    for path in path_list:
+        path = path.resolve()
 
-    files_list = []  # fill with only file paths and no folder paths
+        if path.is_file():
+            logging.debug(f"The path '{path}' is a file path")
+            files_list.append(path)
+
+        if path.is_dir():
+            logging.debug(f"The path '{path}' is a folder path")
+            for child_path in path.iterdir():
+                files_list.append(child_path)
+
+        else:
+            logging.critical("Somehow the path is neither file or directory")
+            logging.critical(f"Please check the path '{path}' and try again")
+            pathstat = path.stat()
+            logging.critical(f"The file info: {pathstat}")
+            exit("EXIT: Error with supplied path")
+
+    logging.debug(f"Following list with paths was created: \n{files_list}\n")
+    logging.debug(f"Following list with paths was created: \n{chr(10).join(map(str, files_list))}\n")
 
     return files_list
-
-def callapi_on_list(files_list: list, base_api_prompt: str):
-
-    # TODO: funktion, 1. param liste mit pfaden - files_list
-    #  - 2. param - basisanfrage an die API - base_api_prompt
-    #  Dann geht die funktion die ganze liste durch und führt fuer jedes item die api anfrage durch
-    #  - Ergebnisse in einer Liste speichern
 
 
 def open_file_list(files_list: list) -> dict:
@@ -67,16 +74,16 @@ def open_file_list(files_list: list) -> dict:
     # This function opens all text files from the supplied path list and saves the contents of the files n a
     # dictionary where the key is the filename and the value the file content
 
-    msg_list = {}
+    msg_dict = {}  # declare empty dict which will be returned by this function
 
-    for path in files_list:
-        filename = path.name()
+    for path in files_list:  # loop over the whole list with filepaths
+        filename = path.name()  # get the file name
         filecontent = open_message(path)  # call the function to open file at supplied path
-        msg_list[filename] = filecontent
+        msg_dict[filename] = filecontent  # create new item in dict, key being filename and value being the content
 
-    logging.debug(f"Following dict with filecontents was created: \n{chr(10).join(map(str, msg_list))}\n")
+    logging.debug(f"Following dict with filecontents was created: \n{chr(10).join(map(str, files_list))}\n")
 
-    return msg_list
+    return msg_dict
 
 
 def open_message(textfile) -> str:
@@ -91,6 +98,43 @@ def open_message(textfile) -> str:
     except FileNotFoundError:
         logging.critical(f"!!! THE FILE {textfile} WAS NOT FOUND, ABORTING EXECUTION !!!")
         exit("Path was not found")
+
+
+def api_calls_on_dict(msg_dict: dict, base_api_prompt: str) -> dict:
+    logging.debug(f"Called function '{api_calls_on_dict.__name__}'")
+    logging.debug(f"First parameter is: '{msg_dict}'")
+    logging.debug(f"Second parameter is: '{base_api_prompt}'")
+
+    api_result_dict = {}  # declare empty dict which will be returned by this function
+
+    for key, value in msg_dict:  # loop over the whole msg_list with key and value of the msg_list
+        response = api_call_completion(base_api_prompt,
+                                       value)  # get the api_call with the base_api_prompt and the value of the call
+        api_result_dict[key] = response  # create new item in dict, that stores the response of the call
+
+    return api_result_dict
+
+
+def get_text_from_response_dict(api_result_dict: dict) -> dict:
+    logging.debug(f"Called function '{get_text_from_response_dict.__name__}' without parameters.")
+    # this function iterates over the given dictionary and gets the actual response text from the api response
+    # return is a dictionary with the filename as key and the textual response as value
+    api_result_text_dict = {}
+
+    for key, value in api_result_dict:
+        api_result_text_dict[key] = api_response_get_text(value)
+
+    return api_result_text_dict
+
+
+def prettyprint_api_text_response_dict(api_result_text_dict: dict):
+    # this function prints all the api response to given files
+    logging.debug(
+        f"Called function '{prettyprint_api_text_response_dict.__name__}' and using '{api_result_text_dict}' as its parameter")
+
+    print("\n\n\n API response: \n")
+    for key, value in api_result_text_dict:
+        print(f"For the file {key} the API responded with {value} \n")
 
 
 def api_response_get_text(response) -> str:
@@ -117,7 +161,7 @@ def api_call_completion(prompt: str, msg_input: str):
 
 
 def setup_logging(verbosity_level):
-    base_loglevel = 30
+    base_loglevel = 30  # base value is only to display warning messages
     verbosity_level = min(verbosity_level, 2)
     loglevel = base_loglevel - (verbosity_level * 10)  # loglevel goes from 10 to 50, intervals of 10
     # logging.DEBUG = 10; logging.CRITICAL = 50
@@ -162,21 +206,22 @@ def main():
     logging.info("Starting...")
     logging.debug(f"The path supplied is: {type(options.path)}")
 
-    msg_to_check = get_paths_list(options.path)
-    if print_text:
-        logging.info("Opening message completed, following text was read: \n")
-        print(msg_to_check)  # print the text back to user
+    # msg_to_check = get_paths_list(options.path)
+    # if print_text:
+    #     logging.info("Opening message completed, following text was read: \n")
+    #     print(msg_to_check)  # print the text back to user
+
+    messages_dict = open_file_list(get_paths_list(options.path))
 
     base_api_prompt = DEFAULT_API_PROMPT
 
     if options.enable_api:
         logging.info("Api call is active")
-        # following is responsible for the api call, disabled to save credits:
-        api_response = api_call_completion(base_api_prompt, msg_to_check)
-        answered_text = api_response_get_text(api_response)
-        print("API response: \n")
-        print(answered_text)
-        logging.debug(f"Full API response \n {api_response}")
+
+        api_responses_dict = api_calls_on_dict(messages_dict, base_api_prompt)
+        api_text_responses = get_text_from_response_dict(api_responses_dict)
+        prettyprint_api_text_response_dict(api_text_responses)
+
     else:
         logging.info(f"The argument '--enable_api' was not supplied, no api call")
         print("No actual api call performed to save credits. \n use argument '--enable_api' to use openai's api ")
