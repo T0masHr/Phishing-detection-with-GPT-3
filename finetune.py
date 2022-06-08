e142a0ce43ecf39ad3bcbee43b0edc5a1b6eb8eb
T0masHr <hrubec.tomas@outlook.com>
2022-06-09 00:12:19 +0200

    little cleanup

--- finetune.py
+++ finetune.py
@@ -11,6 +11,7 @@ DEFAULT_OUTPUT_FILE = "nogit/output.jsonl"
 
 
 def craft_jsonl(output_file: str, prompt: str, msg_list: dict, desired_output: str):
+    # this function transforms the data in the jsonlines format needed by openai for finetuning
     # {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
     # "prompt": "Determine if this email is a phishing email:\n",
     with jsonlines.open(output_file, mode='w') as writer:
@@ -24,10 +25,10 @@ def main():
                                                 "provided all files in the directory are extracted and analysed")
     parser.add_argument("-v", "--verbose", action="count", default=0, dest="verbosity_level",
                         help="set output verbosity: -v for INFO; -vv for DEBUG")
-    parser.add_argument("--api_key", default=DEFAULT_API_KEY,
-                        help="API_KEY for openai, by default the environment variable OPENAI_API_KEY is used")
-    parser.add_argument("--enable_api", action="store_true",
-                        help="enable the call to openai api")
+    # parser.add_argument("--api_key", default=DEFAULT_API_KEY,
+    #                     help="API_KEY for openai, by default the environment variable OPENAI_API_KEY is used")
+    # parser.add_argument("--enable_api", action="store_true",
+    #                     help="enable the call to openai api")
     parser.add_argument("--json_config", default=DEFAULT_JSON_CONFIG,
                         help=f"Change the used api config, by default the following configfile is used: '{DEFAULT_JSON_CONFIG}'")
     parser.add_argument("-o", "--output_file", default=DEFAULT_OUTPUT_FILE,
@@ -46,7 +47,7 @@ def main():
     if args.output_file is DEFAULT_OUTPUT_FILE:
         print(f"No output file supplied, using the default '{DEFAULT_OUTPUT_FILE}'")
 
-    openai.api_key = args.api_key
+    # openai.api_key = args.api_key
     api_config = parse_api_json_config(args.json_config)
     print(f"\nThe following json config was loaded: \n {pformat(api_config)}\n")
     output_file = args.output_file
@@ -62,13 +63,13 @@ def main():
 
     craft_jsonl(output_file, api_config["prompt"], messages_dict, 'This email is not a phishing email.')
 
-    if args.enable_api:
-        print("Api call is active")
-        print("...")
-
-    else:
-        logging.info(f"The argument '--enable_api' was not supplied, no api call")
-        print("No actual api call performed to save credits. \n use argument '--enable_api' to use openai's api ")
+    # if args.enable_api:
+    #     print("Api call is active")
+    #     print("...")
+    #
+    # else:
+    #     logging.info(f"The argument '--enable_api' was not supplied, no api call")
+    #     print("No actual api call performed to save credits. \n use argument '--enable_api' to use openai's api ")
 
 
 if __name__ == '__main__':
