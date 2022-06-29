c70f0cf4a1693f6c9e8061637c0649b5aeaae610
T0masHr <hrubec.tomas@outlook.com>
2022-06-29 09:21:17 +0200

    prepare new modules

---
+++ jsonloader.py
@@ -0,0 +1,15 @@
+
+
+
+def prediction_correct(expected_result, api_result):
+    """
+    Compare the two arguments and determine if the classification performed by the API was correct.
+
+    :param expected_result: The correct classification of the email message.
+    :param api_result: Classification of the message by the API.
+    :return: Boolean, returns true if the  classification is correct.
+    """
+
+
+
+def calculate_accuracy():
